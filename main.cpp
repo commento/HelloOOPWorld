0cb2d75b2052a5cde03065ca1f64f969ff507f53
Riccardo <riccardo@null.net>
2019-07-24 12:26:43 +0200

    convert tab indentation to whitespace

--- main.cpp
+++ main.cpp
@@ -5,86 +5,86 @@
 
 class Character
 {
-	char character;
+    char character;
 public:
 
-	Character() = default;
+    Character() = default;
 
-	Character(char character) : character(character){};
+    Character(char character) : character(character){};
 
-	char getCharacter()
-	{
-		return character;
-	}
+    char getCharacter()
+    {
+        return character;
+    }
 };
 
 class AlphabetLetter : public Character
 {
 
 public:
-	AlphabetLetter(char character) : Character(character) {}
+    AlphabetLetter(char character) : Character(character) {}
 
 };
 
 class Whitespace : public Character
 {
-	static const char character = ' ';
+    static const char character = ' ';
 public:
-	Whitespace() : Character(character) {}
+    Whitespace() : Character(character) {}
 };
 
 class Displayer
 {
 public:
-	Displayer() = default;
+    Displayer() = default;
 
-	template <class T>
-	void display(std::vector<T> string)
-	{	
-			for(auto character : string){
-				std::cout << character.getCharacter();
-			}
-			std::cout << std::endl;
-	}
+    template <class T>
+    void display(std::vector<T> string)
+    {   
+            for(auto character : string){
+                std::cout << character.getCharacter();
+            }
+            std::cout << std::endl;
+    }
 
 };
 
 
 int main(int argc, const char * argv[]) {
 
-	AlphabetLetter H = AlphabetLetter('H');
-	AlphabetLetter e = AlphabetLetter('e');
-	AlphabetLetter l = AlphabetLetter('l');
-	AlphabetLetter o = AlphabetLetter('o');
+    AlphabetLetter H = AlphabetLetter('H');
+    AlphabetLetter e = AlphabetLetter('e');
+    AlphabetLetter l = AlphabetLetter('l');
+    AlphabetLetter o = AlphabetLetter('o');
 
-	Whitespace space = Whitespace();
-	AlphabetLetter O = AlphabetLetter('O');
-	AlphabetLetter P = AlphabetLetter('P');
+    Whitespace space = Whitespace();
+    AlphabetLetter O = AlphabetLetter('O');
+    AlphabetLetter P = AlphabetLetter('P');
 
-	AlphabetLetter W = AlphabetLetter('W');
-	AlphabetLetter r = AlphabetLetter('r');
-	AlphabetLetter d = AlphabetLetter('d');
+    AlphabetLetter W = AlphabetLetter('W');
+    AlphabetLetter r = AlphabetLetter('r');
+    AlphabetLetter d = AlphabetLetter('d');
 
-	std::vector<Character> characters;
-	characters.push_back(H);
-	characters.push_back(e);
-	characters.push_back(l);
-	characters.push_back(l);
-	characters.push_back(o);
-	characters.push_back(space);
-	characters.push_back(O);
-	characters.push_back(O);
-	characters.push_back(P);
-	characters.push_back(space);
-	characters.push_back(W);
-	characters.push_back(o);
-	characters.push_back(r);
-	characters.push_back(l);
-	characters.push_back(d);
+    std::vector<Character> characters;
+    characters.push_back(H);
+    characters.push_back(e);
+    characters.push_back(l);
+    characters.push_back(l);
+    characters.push_back(o);
+    characters.push_back(space);
+    characters.push_back(O);
+    characters.push_back(O);
+    characters.push_back(P);
+    characters.push_back(space);
+    characters.push_back(W);
+    characters.push_back(o);
+    characters.push_back(r);
+    characters.push_back(l);
+    characters.push_back(d);
 
-	Displayer displayer;
+    Displayer displayer;
 
-	displayer.display(characters);
+    displayer.display(characters);
 
     return 0;
 }
